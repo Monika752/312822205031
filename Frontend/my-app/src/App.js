23d1cd890d466c092d1eb6435c39e76747183cee
Monika S <monika17679@gmail.com>
2025-07-17 11:56:47 +0530

    Add files via upload

---
+++ Frontend/my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UrlShortenerForm from './components/UrlShortenerForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <UrlShortenerForm />
+    </div>
+  );
+}
+
+export default App;
